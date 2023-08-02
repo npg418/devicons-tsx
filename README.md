fcd268704b91ffbafd2bc9633659bb0862c91617
NPG418 <narupenguin919@gmail.com>
2023-08-02 21:33:02 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Devicons TSX
+
+Easy to use [Devicons](https://devicon.dev/) in your Fresh app. Just import the icons.  
+This module inspired by [TSX Tabler Icons](https://tabler-icons-tsx.deno.dev/) and [Devicons React](https://devicons-react.vercel.app/).
+
+## Usage
+
+```typescript
+import { AdonisjsOriginal } from 'https://deno.land/x/devicons_tsx/mod.ts'
+```
+
+or
+
+```typescript
+import AdonisjsOriginal from 'https://deno.land/x/devicons_tsx/icons/AdonisjsOriginal.tsx'
+```
+
+Search for icons [here](https://devicons-react.vercel.app/latest).
---
+++ build.ts
@@ -0,0 +1,29 @@
+import DeviconData from 'https://raw.githubusercontent.com/devicons/devicon/master/devicon.json' assert { type: 'json' };
+import camelCase from 'https://deno.land/x/case@2.1.1/camelCase.ts';
+import upperFirstCase from 'https://deno.land/x/case@2.1.1/upperFirstCase.ts';
+
+try {
+  Deno.removeSync('icons', { recursive: true });
+  Deno.removeSync('mod.ts');
+} catch (_) {
+  // ignore
+} finally {
+  Deno.mkdirSync('icons');
+}
+
+const iconsRemote = 'https://raw.githubusercontent.com/devicons/devicon/master/icons';
+const enc = (s: string) => new TextEncoder().encode(s);
+
+for (const iconInfo of DeviconData) {
+  for (const variant of iconInfo.versions.svg) {
+    const name = upperFirstCase(camelCase(iconInfo.name+'-'+variant));
+    Deno.stdout.writeSync(enc(`\n🟥 ${name} building`));
+    const url = `${iconsRemote}/${iconInfo.name}/${iconInfo.name}-${variant}.svg`;
+    const svg = await fetch(url).then((res) => res.text());
+    const jsx = svg.replace(/<svg ([^>]*)>/, '<svg $1 {...props}>');
+    const fc = `export default function ${name}(props: Record<string, unknown>) { return (${jsx}) }`;
+    Deno.writeFileSync(`icons/${name}.tsx`, enc(fc));
+    Deno.writeFileSync('mod.ts', enc(`export { default as ${name} } from './icons/${name}.tsx';\n`), { append: true });
+    Deno.stdout.writeSync(enc(`\x1B[1K\r🟦 ${name} built`));
+  }
+}
