18e44c3cccf31e68f35bc713aa6a3023f9a61c03
NPG418 <narupenguin919@gmail.com>
2023-08-03 12:23:54 +0900

    🔧 Build

--- icons/JuliaPlain.tsx
+++ icons/JuliaPlain.tsx
@@ -1,1 +1,1 @@
-export default function JuliaPlain(props: Record<string, unknown>) { return (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128" {...props}><circle cx="29.1" cy="94.2" r="29.1"/><circle cx="98.9" cy="94.2" r="29.1"/><circle cx="64" cy="33.8" r="29.1"/></svg>) }
+export default function JuliaPlain({ w = 24, h = 24, ...props }) { return (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128" width={w} height={h} {...props}><circle cx="29.1" cy="94.2" r="29.1"/><circle cx="98.9" cy="94.2" r="29.1"/><circle cx="64" cy="33.8" r="29.1"/></svg>) }
--- icons/SolidityPlain.tsx
+++ icons/SolidityPlain.tsx
@@ -1,2 +1,2 @@
-export default function SolidityPlain(props: Record<string, unknown>) { return (<svg viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" fill="#383838" {...props}><path d="M43.322 0L22.756 36.576l20.566 36.561 20.564-36.561h41.143L84.465 0H43.322zm41.342 54.863L64.1 91.424H22.955L43.519 128h41.145l20.58-36.576-20.58-36.561z"/></svg>
+export default function SolidityPlain({ w = 24, h = 24, ...props }) { return (<svg viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" fill="#383838" width={w} height={h} {...props}><path d="M43.322 0L22.756 36.576l20.566 36.561 20.564-36.561h41.143L84.465 0H43.322zm41.342 54.863L64.1 91.424H22.955L43.519 128h41.145l20.58-36.576-20.58-36.561z"/></svg>
 ) }
