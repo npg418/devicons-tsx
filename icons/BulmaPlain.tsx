e5f276b042db8713295c516eeb25ee8116bd1cfc
NPG418 <narupenguin919@gmail.com>
2023-08-04 15:43:58 +0900

    🔧Build

--- icons/BulmaPlain.tsx
+++ icons/BulmaPlain.tsx
@@ -1,1 +1,1 @@
-export default function BulmaPlain({ w = 24, h = 24, ...props }) { return (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128" width={w} height={h} {...props}><path fill="#00D1B2" d="M59.2 0l40 40-24 24 32 31.9L59.4 128l-40-39.9 7.7-56z"/></svg>) }
+export default function BulmaPlain({ w = 64, h = 64, ...props }) { return (<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 128" width={w} height={h} {...props}><path fill="#00D1B2" d="M59.2 0l40 40-24 24 32 31.9L59.4 128l-40-39.9 7.7-56z"/></svg>) }
